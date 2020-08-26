b409a78783920c6e9ec9fbb16500ce73d6e8d93a
Nikk Ogasa <ntogasa@gmail.com>
2020-08-26 09:54:41 -0700

    Add 'about me' page
    - In main.views, create about__view()
    - In main.urls, route requests for 'about' to about_view
    - Create template.about
    - Add link to 'about' in navbar

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('about/', views.about_view, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,10 @@ from django.views import generic
 def home_view(request):
     """Handles requests for the homepage"""
     return render(request, 'home.html')
+
+
+def about_view(request):
+    """Accepts a request and returns the rendered 'about.html' template."""
+    return render(request, 'about.html')
+
+
