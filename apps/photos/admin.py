8bf74cc1fc74d3b3d7581ba52914ae11c5d41f09
Nikk Ogasa <ntogasa@gmail.com>
2020-09-02 10:59:39 -0700

    Create Category model in apps.photos.models
    - register Category in apps.photos.admin

--- apps/photos/admin.py
+++ apps/photos/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('alt', 'date_uploaded', 'status')
     list_filter = ('status',)
 
+
 # Register your models here.
 admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Category, CategoryAdmin)
--- apps/photos/models.py
+++ apps/photos/models.py
@@ -5,10 +5,19 @@ STATUS = (
     (1, 'Publish')
 )
 
-# Create your models here.
+
+class Category(models.Model):
+    """A single CharField which represents the category(s) a photo can fall into"""
+    name = models.CharField(max_length=50)
+
+
 class Photo(models.Model):
+    """The model for photos which are to be displayed in the photo gallery"""
     alt = models.CharField(max_length=500, null=True)
     image = models.ImageField(null=False, blank=False, upload_to='photos')
     date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    categories = models.ManyToManyField('Category', related_name='photos')
+
+
 
