860999951069bf3aa565102ee644634a6d1018cc
Nikk Ogasa <ntogasa@gmail.com>
2020-11-14 15:12:49 -0800

    Order pieces by date descending

--- apps/writing/views.py
+++ apps/writing/views.py
@@ -3,6 +3,6 @@ from .models import WritingPieces
 
 # Create your views here.
 def writing_view(request):
-    writing_pieces = WritingPieces.objects.filter(status=1)
+    writing_pieces = WritingPieces.objects.filter(status=1).order_by('-date_published')
     context = {'writing_pieces': writing_pieces}
     return render(request, 'writing/writing.html', context)
