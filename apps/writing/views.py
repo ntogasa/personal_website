3560a6365825f9acb1b4817506efdb7071620edb
Nikk Ogasa <ntogasa@gmail.com>
2020-11-14 15:16:06 -0800

    Fix date issue

--- apps/writing/views.py
+++ apps/writing/views.py
@@ -3,6 +3,6 @@ from .models import WritingPieces
 
 # Create your views here.
 def writing_view(request):
-    writing_pieces = WritingPieces.objects.filter(status=1).order_by('-date_published')
+    writing_pieces = WritingPieces.objects.filter(status=1).order_by('-date')
     context = {'writing_pieces': writing_pieces}
     return render(request, 'writing/writing.html', context)
