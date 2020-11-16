81f9d3c997fb4e362af9e5743434eba6f4e1c7b2
Nikk Ogasa <ntogasa@gmail.com>
2020-11-16 00:14:52 -0800

    Simplified home page

--- apps/writing/views.py
+++ apps/writing/views.py
@@ -4,5 +4,7 @@ from .models import WritingPieces
 # Create your views here.
 def writing_view(request):
     writing_pieces = WritingPieces.objects.filter(status=1).order_by('-date')
-    context = {'writing_pieces': writing_pieces}
+    featured_pieces = WritingPieces.objects.filter(feature=1).order_by('-date')
+    context = {'writing_pieces': writing_pieces,
+               'featured_pieces': featured_pieces[:2]}
     return render(request, 'writing/writing.html', context)
